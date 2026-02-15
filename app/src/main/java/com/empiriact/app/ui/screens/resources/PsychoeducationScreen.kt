aeb88a62e1a51d667f90ac6ac1de4df897482c3c
Tom <32821177+tomtactom@users.noreply.github.com>
2026-02-15 19:27:37 +0100

    Fix Kotlin unresolved refs in database and psychoeducation screen

--- app/src/main/java/com/empiriact/app/ui/screens/resources/PsychoeducationScreen.kt
+++ app/src/main/java/com/empiriact/app/ui/screens/resources/PsychoeducationScreen.kt
@@ -22,6 +22,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
