d3cd2de784b9d9342606bca7daf00db404945d3f
Tom <32821177+tomtactom@users.noreply.github.com>
2026-03-11 14:01:38 +0100

    Fix onboarding compile error from internal weight import

--- app/src/main/java/com/empiriact/app/ui/screens/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/empiriact/app/ui/screens/onboarding/OnboardingScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
